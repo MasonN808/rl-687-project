7d7a476b63b0112d7832c90b1a73f0eb0f263edc
MasonN808 <mason.naka@gmail.com>
2023-12-07 22:16:26 -0500

    reinforce still not working well on cartpole

--- REINFORCE.py
+++ REINFORCE.py
@@ -108,12 +108,30 @@ def reinforce(env, alpha_theta:float = .01, alpha_w:float = .01, n_episodes=100,
             value_loss.backward()
         average_policy_loss = sum(policy_losses)/len(policy_losses)
         average_value_loss = sum(value_losses)/len(value_losses)
+
+        # policy_losses = []
+        # value_losses = []
+        # for log_prob, value, G in zip(log_probs, values, returns):
+        #     delta = G - value.item()
+        #     # Ignore gamma term since not needed in practice
+        #     policy_loss = -log_prob * delta
+        #     policy_losses.append(policy_loss)
+        #     # Use mean-squared error
+        #     # (value.squeeze() - G)^2 is equivalnet to gradient of delta
+        #     value_loss = (value - G).pow(2)
+        #     value_losses.append(value_loss)
+        # average_policy_loss = sum(policy_losses)/len(policy_losses)
+        # average_value_loss = sum(value_losses)/len(value_losses)
         
         average_policy_losses.append(average_policy_loss.item())
         average_value_losses.append(average_value_loss.item())
 
+        # policy_loss =
+
         policy_optimizer.step()
         baseline_optimizer.step()
+        # policy_loss.backward()
+        # value_loss.backward()
 
     return policy_net, baseline_net, all_total_rewards, average_policy_losses, average_value_losses
 
@@ -125,16 +143,14 @@ class Parameters():
         self.n_episodes = n_episodes
         self.gamma = gamma
 
-def random_tune(env, iterations: int):
+def random_tune(env, iterations: int, n_episodes: int, gamma: float):
     J_values_dict = {}
     best_avg_return = 0
     for i in range(0, iterations):
         print(f'iteration: {i}')
         # Hyperparamter tune via Guassian values
-        alpha_theta = np.random.uniform(.001, .1)
-        alpha_w = np.random.uniform(.001, .1)
-        n_episodes = 1000
-        gamma = .9
+        alpha_theta = np.random.uniform(.001, .002)
+        alpha_w = np.random.uniform(.001, .002)
         _, _, J_values, _, _ = reinforce(env, alpha_theta=alpha_theta, alpha_w=alpha_w, n_episodes=n_episodes, gamma=gamma)
 
         average_return = sum(J_values) / len(J_values)
@@ -198,14 +214,15 @@ def max_lists(list_of_lists):
 
 if __name__=="__main__":
     # Example usage
-    # env_name = "CartPole-v1"
-    # env = gym.make(env_name)
-    env_name = "GW687"
-    env = gw.GW687()
-    alpha_theta = .004
-    alpha_w = .004
-    iterations = 5
-    n_episodes = 500
+    env_name = "CartPole-v1"
+    env = gym.make(env_name)
+    # env_name = "GW687"
+    # env = gw.GW687()
+    # alpha_theta = .001
+    # alpha_w = .001
+    # iterations = 5
+    n_episodes = 2000
+    gamma = .99
 
     DUMP = True
     filename = f'REINFORCE/data/{env_name}/data-{n_episodes}.pkl'
@@ -219,7 +236,7 @@ if __name__=="__main__":
             data = {}
 
         # Your new data
-        values_dict = random_tune(env, iterations=5)
+        values_dict = random_tune(env, iterations=5, n_episodes=n_episodes, gamma=gamma)
 
         # Update the dictionary with the new data
         data.update(values_dict)
@@ -239,6 +256,56 @@ if __name__=="__main__":
     for key, value in top_items:
         print(f"Key: {key.alpha_theta, key.alpha_w, key.n_episodes, key.gamma}, Value: {value}")
         plot(env, env_name, n_iterations=5, alpha_theta=key.alpha_theta, alpha_w=key.alpha_w, n_episodes=key.n_episodes, gamma=key.gamma)
+
+
+    # import matplotlib.pyplot as plt
+    # import matplotlib.animation as animation
+    # from PIL import ImageFont, ImageDraw, Image
+    # import numpy as np
+    # import cv2
+
+    # policy_net, baseline_net, _, _, _ = reinforce(env, .001, .001, 1000, .99)
+
+    # fig = plt.figure()
+    # ACTION_SPACE = [0,1]
+    # env = gym.make("CartPole-v0")
+    # state = env.reset()
+    # ims = []
+    # rewards = []
+    # for step in range(500):
+    #     # env.render()
+    #     img = env.render(render_mode='rgb_array')
+    #     state_tensor = from_numpy(state).float().unsqueeze(0)
+    #     action_probs = policy_net(state_tensor)
+    #     dist = distributions.Categorical(action_probs)
+    #     action = dist.sample()
+    #     print(action)
+    #     state,reward,done,_ = env.step(action.item())
+    #     print(reward)
+    #     rewards.append(reward)
+    #     cv2_im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     pil_im = Image.fromarray(cv2_im_rgb)
+
+    #     draw = ImageDraw.Draw(pil_im)
+
+    #     # Choose a font
+    #     font = ImageFont.truetype("Roboto-Regular.ttf", 20)
+
+    #     # Draw the text
+    #     draw.text((0, 0), f"Step: {step} Action : {action} Reward: {reward} Total Rewards: {np.sum(rewards)} done: {done}", font=font,fill="#000000")
+
+    #     # Save the image
+    #     img = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
+    #     # img = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2GRAY)
+    #     im = plt.imshow(img, animated=True)
+    #     ims.append([im])
+    # env.close()    
+
+    # Writer = animation.writers['pillow']
+    # writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    # im_ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=3000,
+    #                                     blit=True)
+    # im_ani.save('cp_train.gif', writer=writer)
 
     # Plot rewards over episodes
     # plt.plot(all_total_rewards)
